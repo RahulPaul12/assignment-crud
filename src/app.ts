8548d151331abe67acbe68999385eccb39207b1c
your_username <rahulpaul199914@gmail.com>
2023-11-23 16:16:27 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { userRoutes } from './app/modules/user/user.route';
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+
+//application routes
+
+app.use('/api', userRoutes)
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
